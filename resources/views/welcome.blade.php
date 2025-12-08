f5c798d83f9f45cab129e850c74c61f92f8fb1ad
Bebe Kote <lifetroll6@gmail.com>
2025-12-08 14:23:51 +0200

    fixed the grid layout for mobile on the featured products

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,8 +36,7 @@
 
         <!-- Featured Products Section -->
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-            <h3 class="text-3xl font-bold text-pearl-900 mb-8 text-center font-info">{{ __('welcome.featured_products') }}</h3>
-            {{-- here comes the showcase --}}
+            <h3 class="text-3xl font-bold mb-8 text-center font-info">{{ __('welcome.featured_products') }}</h3>
             <livewire:product-showcase.product-showcase/>
         </div>
 
