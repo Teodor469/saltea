351877fd761bc7977865ae0e7db9a35e401fa43a
Bebe Kote <lifetroll6@gmail.com>
2025-12-18 09:49:25 +0200

    changed the favicon and created a readme file

--- resources/views/components/layouts/public.blade.php
+++ resources/views/components/layouts/public.blade.php
@@ -6,6 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $title ?? config('app.name', 'Saltea') }}</title>
 
+    <!-- Favicon -->
+    <link rel="icon" href="/images/logos/Saltea.png" sizes="any">
+    <link rel="icon" href="/images/logos/Saltea.png" type="image/png">
+    <link rel="apple-touch-icon" href="/images/logos/Saltea.png">
+
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
